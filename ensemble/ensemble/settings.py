d17532aed16e26642d3aff65a30b2bff22a52aa4
Roberto Loja <robertoloja@gmail.com>
2021-12-19 12:11:02 -0500

    More heroku deploy details

--- ensemble/ensemble/settings.py
+++ ensemble/ensemble/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
